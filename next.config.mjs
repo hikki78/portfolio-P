5712264c015286031e89dee8abae892f1fb3dfb9
hikki78 <meghadrimukherjee680@gmail.com>
2024-03-02 17:51:09 +0530

    next.config changes

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,18 @@
 // next.config.mjs
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.experiments = {
+      asyncWebAssembly: true,
+    };
+
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: "webassembly/async",
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
