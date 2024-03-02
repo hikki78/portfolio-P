36cce925ae2f7364cc381e73e85b23187ebc2669
hikki78 <meghadrimukherjee680@gmail.com>
2024-03-02 19:31:45 +0530

    alright thak gya hu bhai

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,15 @@
-// _app.tsx
-import { useRouter } from "next/router";
-import { ThemeProvider } from "next-themes";
-import type { AppProps } from "next/app";
-import { AnimatePresence } from "framer-motion";
-import Head from "next/head";
-import Script from "next/script";
-import { Toaster } from "react-hot-toast";
+import { useRouter } from 'next/router'
+import { ThemeProvider } from 'next-themes'
+import type { AppProps } from 'next/app'
+import { AnimatePresence } from 'framer-motion'
+import Head from 'next/head'
+import Script from 'next/script'
+import { Toaster } from 'react-hot-toast'
 
-import "../styles/globals.scss";
-import BackToTopButton from "../components/BackToTopButton";
-import ProfileCard from "../components/ProfileCard"; // import the ProfileCard component
-
+import '../styles/globals.scss'
+import BackToTopButton from '../components/BackToTopButton'
 function App({ Component, pageProps }: AppProps): JSX.Element {
-  const router = useRouter();
+  const router = useRouter()
 
   return (
     <ThemeProvider defaultTheme="dark">
@@ -21,12 +18,11 @@ function App({ Component, pageProps }: AppProps): JSX.Element {
       </Head>
       <Toaster position="top-right" reverseOrder={false} />
       <BackToTopButton />
-      <ProfileCard /> {/* use the ProfileCard component */}
       <AnimatePresence initial={false}>
         <Component {...pageProps} key={router.route} />
       </AnimatePresence>
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,9 @@ import { SocialIcons } from "@components/SocialIcons";
 import { MainHeader } from "@components/MainHeader";
 import React, { useEffect, useRef } from "react";
 import type { NextPage } from "next";
+import Head from "next/head";
+// import useSWR from 'swr'
+
 import NavBar from "../components/NavBar";
 import styles from "../styles/mainheader.module.scss";
 import IconsComponent from "../components/IconsComponent";
@@ -9,7 +12,6 @@ import BreakComponent from "../components/BreakComponent";
 import ProjectsComponent from "../components/projects/ProjectsComponent";
 import MessageComponent from "../components/message/MessageComponent";
 import { Footer } from "../components/Footer";
-import ProfileCard from "../components/ProfileCard"; // import the ProfileCard component
 
 const Home: NextPage = () => {
   const parentRef = useRef(null);
@@ -19,18 +21,28 @@ const Home: NextPage = () => {
     parentRef.current.scrollIntoView();
   };
 
+  // const fetcher = (url: RequestInfo | URL) => fetch(url).then(r => r.json())
+  // const { data } = useSWR('/api/spotify', fetcher)
+  // console.log(data)
+
   return (
     <>
+      <Head>
+        <meta
+          property="og:image"
+          content="https://www.canva.com/design/DAF-XbgSe4E/Djmsg-RF6446Bgvy38IKCA/edit?utm_content=DAF-XbgSe4E&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+        />
+      </Head>
       <main className={styles.header_container}>
         <NavBar />
         <MainHeader />
         <SocialIcons scrollFunction={scrollFunction} />
         <BreakComponent marginTop="4rem" />
         <IconsComponent />
-        <ProfileCard /> {/* use the ProfileCard component */}
         <ProjectsComponent />
         <BreakComponent marginTop="2rem" />
         <MessageComponent ref={parentRef} />
+
         <Footer />
       </main>
     </>
